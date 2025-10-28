0d437ee215aa178698ba42a709dbbd06d2669196
57U <ming57u@foxmail.com>
2025-10-28 14:42:04 +0800

    update

--- plugins/ViewDialog.cs
+++ plugins/ViewDialog.cs
@@ -55,7 +55,9 @@ public class ViewDialog : Plugin
                     }
                     else if (item.Role == ZhipuAi.ASSISTANT) { 
                         var item2 = item as AssistantMessage;
-                        sb.Append("assistant: " + ConstraintLength(item.Content.Trim()));
+                        sb.Append("assistant: " + ConstraintLength(item.Content??""));
+                        
+                        
                         if(item2?.ToolCalls !=null && item2.ToolCalls.Count>0)
                         {
                             foreach(var i in item2.ToolCalls)
