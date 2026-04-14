98a0ddce2e6435264d6dd095ae0bb260f4a7d11b
57U <ming57u@foxmail.com>
2026-04-14 21:13:23 +0800

    refactor: extract plugin loading logic into separate file
    Move plugin-related fields and methods from Logic.cs to Logic.Plugins.cs to improve separation of concerns and maintainability. This isolates the plugin system from the core bot logic.

---
+++ MerryBot/Logic.Plugins.cs
@@ -0,0 +1,139 @@
+using BotPlugin;
+using CommonLib;
+using System.Reflection;
+using Tomlyn.Model;
+
+namespace MerryBot;
+
+internal partial class Logic
+{
+    private readonly List<PluginInfo> plugins = new();
+    private MainPlugin? mainPlugin;
+    private IEnumerable<Action>? _pluginsDisposeActions;
+
+    private static List<long> QqGroupIDs
+    {
+        get
+        {
+            return ConfigManager.Instance.QqGroups;
+        }
+    }
+
+    private static List<(Type type, PluginTag attribute)> FindPlugins()
+    {
+        List<(Type type, PluginTag attribute)> list = [];
+        Assembly assembly = Assembly.GetAssembly(typeof(Plugin))!;
+        foreach (Type type in assembly.GetTypes().Append(typeof(MainPlugin)))
+        {
+            PluginTag attribute = type.GetCustomAttribute<PluginTag>()!;
+            if (attribute != null && !attribute.IsIgnore)
+            {
+                list.Add((type, attribute));
+            }
+        }
+        return list;
+    }
+
+    private void LoadPlugins()
+    {
+        var allPlugins = FindPlugins();
+        allPlugins.Sort((a, b) =>
+        {
+            return a.attribute.Priority.CompareTo(b.attribute.Priority);
+        });
+        PluginInitializer<Plugin> pluginInitializer = new();
+        Dictionary<Type, PluginInterop> pluginInteropMap = new();
+        logger.Debug($"find plugin: {string.Join(",", allPlugins.Select(p => p.attribute.Id))}");
+        foreach (var (type, attribute) in allPlugins)
+        {
+            try
+            {
+                if (!ConfigManager.Instance.Variables.TryGetValue(attribute.Id, out var pluginVars))
+                {
+                    pluginVars = new TomlTable();
+                    ConfigManager.Instance.Variables[attribute.Id] = pluginVars;
+                }
+                var interop = new PluginInterop(
+                        new PluginLogger(attribute.Id),
+                        QqGroupIDs,
+                        () => plugins,
+                        new PluginStorage(
+                            (s) => PluginStorageDatabase.StorePluginData(attribute.Id, s),
+                            () => PluginStorageDatabase.GetPluginData(attribute.Id)
+                            ),
+                        botClient,
+                        pluginVars,
+                        Shutdown,
+                        AuthorizedUser,
+                        CommandLineArguments,
+                        ConfigManager.Save,
+                        botClient.PathPrefix,
+                        f => OnRawGroupMessageReceived += f
+                        );
+                pluginInteropMap.Add(type, interop);
+                if (type == typeof(MainPlugin))
+                {
+                    pluginInitializer.AddDependency(type, new List<object> { this, interop });
+                }
+                else
+                {
+                    pluginInitializer.AddDependency(type, new List<object> { interop });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, $"the plugin {attribute.Id} can not be loaded");
+            }
+        }
+        try
+        {
+            pluginInitializer.InitializeAll();
+        }
+        catch (Exception e)
+        {
+            logger.Fatal(e);
+        }
+        _pluginsDisposeActions = pluginInitializer.GetDisposeActions();
+        IEnumerable<(Plugin? pluginInstance, PluginTag attribute)> allPluginInstance
+            = allPlugins.Select(p => (pluginInitializer.GetInstance(p.type), p.attribute));
+        foreach (var (pluginInstance, attribute) in allPluginInstance)
+        {
+            if (pluginInstance != null)
+            {
+                plugins.Add(
+                    new PluginInfo(
+                    pluginInstance,
+                    attribute,
+                    pluginInteropMap[pluginInstance.GetType()]
+                    )
+                );
+            }
+
+        }
+        mainPlugin = pluginInitializer.GetInstance<MainPlugin>();
+
+        foreach (var i in plugins)
+        {
+            i.Instance.OnLoaded().ContinueWith(task =>
+            {
+                if (task.Exception != null)
+                {
+                    logger.Error($"the plugin {i.PluginTag.Id} OnLoaded failed: {task.Exception}");
+                }
+            });
+        }
+    }
+
+    public void Shutdown(int exitCode = 0)
+    {
+        foreach (var dispose in _pluginsDisposeActions!)
+        {
+            dispose();
+        }
+        PluginStorageDatabase.Dispose();
+        botClient.Close();
+        NLog.LogManager.Shutdown();
+        Environment.Exit(exitCode);
+    }
+}
--- MerryBot/Logic.cs
+++ MerryBot/Logic.cs
@@ -3,9 +3,7 @@ using CommonLib;
 using DataProvider;
 using NapcatClient;
 using NapcatClient.MessageType;
-using System.Reflection;
 using System.Runtime.InteropServices;
-using Tomlyn.Model;
 
 namespace MerryBot;
 
@@ -13,19 +11,10 @@ internal partial class Logic
 {
     readonly BotClient botClient;
     private readonly DataProvider.PluginStorageDatabase PluginStorageDatabase;
-    private readonly List<PluginInfo> plugins = new();
     private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
     public static long AuthorizedUser { get { return ConfigManager.Instance.AuthorizedUser; } }
     readonly string[] CommandLineArguments = Environment.GetCommandLineArgs();
-    private MainPlugin? mainPlugin;
 
-    private static List<long> QqGroupIDs
-    {
-        get
-        {
-            return ConfigManager.Instance.QqGroups;
-        }
-    }
     public Logic(BotClient botClient, string dbPath)
     {
         this.botClient = botClient;
@@ -133,127 +122,4 @@ internal partial class Logic
         OnGroupMessage(groupId, span, data);
     }
 
-    private static List<(Type type, PluginTag attribute)> FindPlugins()
-    {
-        List<(Type type, PluginTag attribute)> list = [];
-        Assembly assembly = Assembly.GetAssembly(typeof(Plugin))!;
-        foreach (Type type in assembly.GetTypes().Append(typeof(MainPlugin))) // add MainPlugin
-        {
-            PluginTag attribute = type.GetCustomAttribute<PluginTag>()!;
-            if (attribute != null && !attribute.IsIgnore)
-            {
-                list.Add((type, attribute));
-            }
-        }
-        return list;
-    }
-    private IEnumerable<Action>? _pluginsDisposeActions;
-    private void LoadPlugins()
-    {
-        var allPlugins = FindPlugins();
-        //sort by priority
-        allPlugins.Sort((a, b) =>
-        {
-            return a.attribute.Priority.CompareTo(b.attribute.Priority);
-        });
-        PluginInitializer<Plugin> pluginInitializer = new();
-        Dictionary<Type, PluginInterop> pluginInteropMap = new();
-        logger.Debug($"find plugin: {string.Join(",", allPlugins.Select(p => p.attribute.Id))}");
-        foreach (var (type, attribute) in allPlugins)
-        {
-            try
-            {
-                // 获取或创建该插件的命名空间字典
-                if (!ConfigManager.Instance.Variables.TryGetValue(attribute.Id, out var pluginVars))
-                {
-                    pluginVars = new TomlTable();
-                    ConfigManager.Instance.Variables[attribute.Id] = pluginVars;
-                }
-                var interop = new PluginInterop(
-                        new PluginLogger(attribute.Id),
-                        QqGroupIDs,
-                        () => plugins,
-                        new PluginStorage(
-                            (s) => PluginStorageDatabase.StorePluginData(attribute.Id, s),
-                            () => PluginStorageDatabase.GetPluginData(attribute.Id)
-                            ),
-                        botClient,
-                        pluginVars,
-                        Shutdown,
-                        AuthorizedUser,
-                        CommandLineArguments,
-                        ConfigManager.Save,
-                        botClient.PathPrefix,
-                        f => OnRawGroupMessageReceived += f
-                        );
-                pluginInteropMap.Add(type, interop);
-                if (type == typeof(MainPlugin))
-                {
-                    pluginInitializer.AddDependency(type, new List<object> { this, interop });
-                }
-                else
-                {
-                    pluginInitializer.AddDependency(type, new List<object> { interop });
-                }
-
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex, $"the plugin {attribute.Id} can not be loaded");
-            }
-        }
-        //initialize
-        try
-        {
-            pluginInitializer.InitializeAll();
-        }
-        catch (Exception e)
-        {
-            logger.Fatal(e);
-        }
-        _pluginsDisposeActions = pluginInitializer.GetDisposeActions();
-        IEnumerable<(Plugin? pluginInstance, PluginTag attribute)> allPluginInstance
-            = allPlugins.Select(p => (pluginInitializer.GetInstance(p.type), p.attribute));
-        foreach (var (pluginInstance, attribute) in allPluginInstance)
-        {
-            if (pluginInstance != null)
-            {
-                plugins.Add(
-                    new PluginInfo(
-                    pluginInstance,
-                    attribute,
-                    pluginInteropMap[pluginInstance.GetType()]
-                    )
-                );
-            }
-
-        }
-        mainPlugin = pluginInitializer.GetInstance<MainPlugin>();
-
-        //加载插件的OnLoaded函数
-        foreach (var i in plugins)
-        {
-            i.Instance.OnLoaded().ContinueWith(task =>
-            {
-                if (task.Exception != null)
-                {
-                    logger.Error($"the plugin {i.PluginTag.Id} OnLoaded failed: {task.Exception}");
-                }
-            });
-        }
-    }
-    /// <summary>
-    /// save data and shutdown
-    /// </summary>
-    public void Shutdown(int exitCode = 0)
-    {
-        foreach (var dispose in _pluginsDisposeActions!)
-        {
-            dispose();
-        }
-        PluginStorageDatabase.Dispose();
-        botClient.Close();
-        NLog.LogManager.Shutdown();
-        Environment.Exit(exitCode);
-    }
 }
